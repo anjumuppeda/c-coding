7bb7e4626f9929aaebb57548ee0c245529a383b6
Anju Muppeda <1213anjusri@gmail.com>
2025-04-29 01:01:51 +0530

    Update caculator.c

--- calculator/caculator.c
+++ calculator/caculator.c
@@ -6,10 +6,10 @@ int main()
     scanf("%d",&a);
     printf("enter the second number\n");
     scanf("%d",&b);
-    printf("%d+%d=%d\n",a,b,sum);
-    printf("%d-%d=%d\n",a,b,diff);
-    printf("%d*%d=%d\n",a,b,mul);
-    printf("%d/%d=%d\n",a,b,div);
-    printf("%d%d=%d\n"a,b,rem);
+    printf("%d+%d=%d\n",a,b,a+b);
+    printf("%d-%d=%d\n",a,b,a-b);
+    printf("%d*%d=%d\n",a,b,a*b);
+    printf("%d/%d=%d\n",a,b,a/b);
+    printf("%d%d=%d\n",a,b,a%b);
     return 0;
 }
