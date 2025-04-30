4e16d66b5d06a974069ed230a30e15003cc956e4
Anju Muppeda <1213anjusri@gmail.com>
2025-04-30 22:54:40 +0530

    Update caculator.c

--- calculator/caculator.c
+++ calculator/caculator.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 int main()
 {
-    int a,b, sum,diff,mul,div,rem;
+    int a,b;
     printf("enter the first number\n");
     scanf("%d",&a);
     printf("enter the second number\n");
